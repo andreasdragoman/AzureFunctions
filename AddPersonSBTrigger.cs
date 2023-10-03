297aa4217ae8cf353537eb42d563ff96ff23f0d4
Andreas Dragoman <andreas.dragoman@ness.com>
2023-10-03 11:35:08 +0300

    add person trigger upd to use cosmos db and commented default cosmostrigger

--- AddPersonSBTrigger.cs
+++ AddPersonSBTrigger.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Text;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Documents.SystemFunctions;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Person.Function
 {
@@ -9,11 +13,26 @@ namespace Person.Function
     {
         [FunctionName("AddPersonSBTrigger")]
         public void Run(
-            [ServiceBusTrigger("personqueue", Connection = "SBConnection")]
-            string myQueueItem,
+            [ServiceBusTrigger("add-person-queue", Connection = "SBConnection")]
+            ServiceBusReceivedMessage myQueueItem,
+            [CosmosDB(
+                databaseName: "MainDatabase",
+                collectionName: "Persons",
+                ConnectionStringSetting = "CosmosDbConnectionString"
+            )]
+            out dynamic document,
             ILogger log)
         {
-            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            Person person = JsonConvert.DeserializeObject<Person>(Encoding.UTF8.GetString(myQueueItem.Body));
+            document = new { personId = Guid.NewGuid(), firstName = person.FirstName, lastName = person.LastName };
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem.Body}");
         }
+    }
+
+    public class Person 
+    {
+        //public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
